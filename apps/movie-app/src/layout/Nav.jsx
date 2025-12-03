276d8ce8e2cf88d913c61be992c319e14b9a56c1
migugin <xiahtic73@gmail.com>
2025-12-03 14:16:56 +0900

    [feat] nav 생성 - scss 라이브러리 추가 - vite alias 설정

--- apps/movie-app/src/layout/Nav.jsx
+++ apps/movie-app/src/layout/Nav.jsx
@@ -1,8 +1,38 @@
+import "@/style/layout/nav.scss";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
 function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
   return (
-    <>
-      Nav
-    </>
+    <nav className={`nav ${show && "nav__black"}`}>
+      <Link to="/" className="nav__logo">
+        <img
+          alt="Netflix logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        />
+      </Link>
+      <img
+        alt="User logged"
+        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+        className="nav__avatar"
+      />
+    </nav>
   );
 }
 
