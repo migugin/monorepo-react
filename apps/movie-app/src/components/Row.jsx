1870a3386e3649c8e5b32fdbf249ba892b6d4220
migugin <xiahtic73@gmail.com>
2025-12-04 16:28:18 +0900

    [feat] 동영상 재생 기능

---
+++ apps/movie-app/src/components/Row.jsx
@@ -0,0 +1,45 @@
+import axios from "@/api/axios";
+import { useEffect, useState } from "react";
+import "@/style/components/Row.scss";
+
+function Row({ title, id, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovieData() {
+      const request = await axios.get(fetchUrl);
+
+      setMovies(request.data.results);
+    }
+
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  return (
+    <section className="row">
+      <h2 className="row__title">{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+
+        <div className="row__posters" id={id}>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+              className={`row__poster ${isLargeRow && "row__poster-large"}`}
+            />
+          ))}
+        </div>
+
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Row;
